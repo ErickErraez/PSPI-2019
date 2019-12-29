4b4acc088b77ea3c40613cf042518345bfd31115
alejodaniel <adf.coronel@yavirac.edu.ec>
2019-12-29 09:06:12 -0500

    sa

--- Server/knexfile.js
+++ Server/knexfile.js
@@ -4,7 +4,7 @@ const databaseData = {
     connection: process.env.CONNECTION_DB || {
         host: 'localhost',
         port: '3306',
-        user: 'prueba',
+        user: 'iepi',
         password: '123456789',
         database: 'pspi'
     }
