0ca81fa690bbb741b6a27adbd99c2de0365af287
RodrigoDiaz10 <raa.diaz@yavirac.edu.ec>
2020-01-13 22:41:41 -0500

    prueba de subida

--- Web/src/app/layout/student-proyect/student-proyect.component.ts
+++ Web/src/app/layout/student-proyect/student-proyect.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-student-proyect',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentProyectComponent implements OnInit {
 
-  constructor() { }
+  uploadedFiles: Array < File > ;
+
+  constructor(private http: HttpClient) {
+
+  }
 
   ngOnInit() {
+
+  }
+
+  fileChange(element) {
+      this.uploadedFiles = element.target.files;
+  }
+
+  upload() {
+      let formData = new FormData();
+      for (var i = 0; i < this.uploadedFiles.length; i++) {
+          formData.append("uploads[]", this.uploadedFiles[i], this.uploadedFiles[i].name);
+      }
+      this.http.post('/api/upload', formData)
+          .subscribe((response) => {
+              console.log('response received is ', response);
+          })
   }
 
 }
