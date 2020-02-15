52addf4ec3929d87c60419334de5aaefaee03d66
alejodaniel <adf.coronel@yavirac.edu.ec>
2020-02-15 15:34:22 -0500

    quitar cond

--- Server/database/seeds/4.TipoEvaluaciones.js
+++ Server/database/seeds/4.TipoEvaluaciones.js
@@ -4,11 +4,11 @@ exports.seed = function (knex, Promise) {
         .then(function () {
             // Inserts seed entries
             return knex('TipoEvaluaciones').insert([
-                {nombre: 'Entregable 1'},
-                {nombre: 'Entregable 2'},
-                {nombre: 'Defensa 1'},
-                {nombre: 'Defensa 2'},
-                {nombre: 'Nota Final'}
+                {tipo: 'Entregable 1'},
+                {tipo: 'Entregable 2'},
+                {tipo: 'Defensa 1'},
+                {tipo: 'Defensa 2'},
+                {tipo: 'Nota Final'}
             ]);
         });
 };
